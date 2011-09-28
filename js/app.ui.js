6bd4430ffe27776ae182c6d5857c02fe9a8fd86a
stephband <stephband@googlemail.com>
2011-09-28 23:12:45 +0200

    Adds header truncate and more button with animation. It's rough and ready. complex html in the header will break it.

--- js/app.ui.js
+++ js/app.ui.js
@@ -1,3 +1,63 @@
 // Stop scroll from escaping and scrolling the outer document (the webdoc page).
 
-jQuery(document).blockScroll();
+jQuery(document)
+.blockScroll()
+.ready(function() {
+	// If the header is too tall, truncate its contents and add a
+	// more button. This is quick and dirty - it will fail if the
+	// header html is complex.
+	
+	var header = jQuery('#header'),
+	    height = header.height(),
+	    more = '<a href="#toggle">more ▾</a>',
+	    less = '<a href="#toggle">close ▴</a>',
+	    textLength = 80,
+	    openHtml, closeHtml, openHeight, closeHeight, children,
+	    length, last, truncHtml, state;
+	
+	// 72 is 4 line of text at 1.5em line-height.
+	if (height > 72) {
+		// Store the full header html. We need it later.
+		openHtml = [header.html(), less].join('');
+		openHeight = header.outerHeight() + 36;
+		state = false;
+		
+		while (header.height() > 72) {
+			children = header.children();
+			length = children.length;
+			last = children.last().remove();
+		}
+		
+		// Truncate text in the last child
+		truncHtml = [last.html().slice(0, textLength), '&hellip;'].join('');
+		
+		last
+		.html(truncHtml)
+		.css({ marginBottom: 0 });
+		
+		header
+		.append(last)
+		.append(more);
+		
+		closeHtml = header.html();
+		closeHeight = header.outerHeight();
+		
+		header
+		.delegate('a[href="#toggle"]', 'mousedown touchstart', function(e){
+			console.log('nahnah');
+			
+			if (state) {
+				header
+				.css({ height: closeHeight })
+				.html(closeHtml);
+			}
+			else {
+				header
+				.css({ height: openHeight })
+				.html(openHtml);
+			}
+			
+			state = !state;
+		});
+	}
+})
